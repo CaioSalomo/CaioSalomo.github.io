5b773ed71fc0551f8673eb87b133b42f0d422eaf
CaioSalomo <118933012+CaioSalomo@users.noreply.github.com>
2022-12-22 18:02:52 -0300

    Finish Script

---
+++ Projeto/interactive-rating-component-main/Assets/script.js
@@ -0,0 +1,34 @@
+var selecionado= false;
+function PegaVal(valor){
+    document.getElementById("js").innerHTML =`<h4 >You selected ${valor} out of 5</h4>`;
+    selecionado=true 
+   
+}
+function Mostra(Tela,Tela1){
+    var display=document.getElementById(Tela).style.display
+    var display1=document.getElementById(Tela1).style.display
+    if(selecionado){
+        if(display=="none"){
+            document.getElementById(Tela1).style.display='block'
+        }
+        else{
+            document.getElementById(Tela1).style.display='none'  
+        }
+        if(display1=="block"){
+            document.getElementById(Tela).style.display='none'
+
+        }
+        else{
+            document.getElementById(Tela).style.display='flex'
+        }
+    }
+    else{
+        alert("Selecione alguma Nota")
+    }
+
+
+  
+
+    
+}
+
