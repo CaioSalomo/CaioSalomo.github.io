f7620540729a5fce6ce6b7dce467dad597a8c522
CaioSalomo <118933012+CaioSalomo@users.noreply.github.com>
2022-12-22 22:17:50 -0300

    JoKenPô

--- Projeto/JOKENPO/Assets/script.js
+++ Projeto/JOKENPO/Assets/script.js
@@ -1,6 +1,7 @@
+var Vit=0
+var Der=0
 function Po(valor){
-    var Vit=0
-    var Der=0
+   
     var option = parseInt(Math.random()*(4-1)+ 1)
     document.getElementById("ImgP").src=`/Assets/Images/${valor}.png`;
     document.getElementById("ImgE").src=`/Assets/Images/${option}.png`;
@@ -11,32 +12,41 @@ function Po(valor){
             switch(option){
             case 2:
                 document.getElementById("result").innerHTML="Derrota"
+                Der++
                 break
             case 3:
                 document.getElementById("result").innerHTML="Vitoria"
+                Vit++
+                break
             }
             break;
         case 2:
              switch(option){
                 case 1:
                     document.getElementById("result").innerHTML="Vitoria"
+                    Vit++
                     break
                 case 3:
                     document.getElementById("result").innerHTML="Derrota"
+                    Der++
+                    break
                 }
                 break;
         case 3:
             switch(option){
                 case 1:
                     document.getElementById("result").innerHTML="Derrota"
+                    Der++
                     break
                 case 2:
                     document.getElementById("result").innerHTML="Vitoria"
+                    Vit++
                     break
                 }
                 break;
         }
-
+        document.getElementById("Vit").innerHTML=`Vitorias : ${Vit}`
+        document.getElementById("Der").innerHTML=`Derrotas : ${Der}`
 
 }
 
