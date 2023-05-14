34aa377035a9a5a2ac1d78f3c5550a52862ccf43
Caio Vitor Ribeiro Salomão Araújo <118933012+CaioSalomo@users.noreply.github.com>
2023-05-14 17:06:19 -0300

    Atualização do Site
    bonzin

--- Projeto/JOKENPO/Assets/script.js
+++ Projeto/JOKENPO/Assets/script.js
@@ -3,8 +3,8 @@ var Der=0
 function Po(valor){
    
     var option = parseInt(Math.random()*(4-1)+ 1)
-    document.getElementById("ImgP").src=`/Assets/Images/${valor}.png`;
-    document.getElementById("ImgE").src=`/Assets/Images/${option}.png`;
+    document.getElementById("ImgP").src=`./Assets/Images/${valor}.png`;
+    document.getElementById("ImgE").src=`./Assets/Images/${option}.png`;
     console.log(option)
     document.getElementById("result").innerHTML="Empate"
     switch(valor){
