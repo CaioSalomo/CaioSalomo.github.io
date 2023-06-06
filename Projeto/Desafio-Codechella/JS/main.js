e5916357a8934ec4af7a37e5d2822ffacd8b7a54
Caio Vitor Ribeiro Salomão Araújo <118933012+CaioSalomo@users.noreply.github.com>
2023-06-06 17:21:37 -0300

    New Page A Experiencia Projeto Chahela

--- Projeto/Desafio-Codechella/JS/main.js
+++ Projeto/Desafio-Codechella/JS/main.js
@@ -13,7 +13,7 @@ function mostraMenu(controle){
 }
 
 const imagens = document.querySelectorAll('[data-imagem]');
-
+if (document.title==="CodeChella"){
 function atualizarImagem(imagem) {
   const dataImagem = imagem.getAttribute("data-imagem");
   const larguraTela = window.innerWidth;
@@ -21,51 +21,113 @@ function atualizarImagem(imagem) {
   if (larguraTela < 760) {
     switch (dataImagem) {
       case "banner":
-        imagem.src = "./Assets/Tema-Boreal/Mobile/Imagens/1 - Banner - Homepage.png";
+        imagem.src = "../Assets/Tema-Boreal/Mobile/Imagens/1 - Banner - Homepage.png";
         imagem.alt = "Banner(Tablet)";
         break;
       case "homestart":
-        imagem.src = './Assets/Tema-Boreal/Mobile/Imagens/1 - Homepage1.png';
+        imagem.src = '../Assets/Tema-Boreal/Mobile/Imagens/1 - Homepage1.png';
         imagem.alt = "HomePage(Tablet))";
         break;
       case "homeend":
-        imagem.src = "./Assets/Tema-Boreal/Mobile/Imagens/1 - Homepage2.png";
+        imagem.src = "../Assets/Tema-Boreal/Mobile/Imagens/1 - Homepage2.png";
         imagem.alt = "Homepage2(Tablet)";
         break;
     }
   } else if (larguraTela<1400) {
     switch (dataImagem) {
       case "banner":
-        imagem.src = "./Assets/Tema-Boreal/Tablet/Imagens/1 - Banner- Homepage.png";
+        imagem.src = "../Assets/Tema-Boreal/Tablet/Imagens/1 - Banner- Homepage.png";
         imagem.alt = "Banner(Tablet)";
         break;
       case "homestart":
-        imagem.src = './Assets/Tema-Boreal/Tablet/Imagens/1 - Homepage1.png';
+        imagem.src = '../Assets/Tema-Boreal/Tablet/Imagens/1 - Homepage1.png';
         imagem.alt = "HomePage(Tablet)";
         break;
       case "homeend":
-        imagem.src = "./Assets/Tema-Boreal/Tablet/Imagens/1 - Homepage2.png";
+        imagem.src = "../Assets/Tema-Boreal/Tablet/Imagens/1 - Homepage2.png";
         imagem.alt = "Homepage2(Tablet)";
         break;
     }
   } else if (larguraTela>1400) {
     switch (dataImagem) {
       case "banner":
-        imagem.src = "./Assets/Tema-Boreal/Desktop/Imagens/1 - Banner - homepage.png";
+        imagem.src = "../Assets/Tema-Boreal/Desktop/Imagens/1 - Banner - homepage.png";
         imagem.alt = "Banner(Desktop)";
         break;
       case "homestart":
-        imagem.src = './Assets/Tema-Boreal/Desktop/Imagens/1 - Imagem - homepage1.png';
+        imagem.src = '../Assets/Tema-Boreal/Desktop/Imagens/1 - Imagem - homepage1.png';
         imagem.alt = "HomePage(Desktop)";
         break;
       case "homeend":
-        imagem.src = "./Assets/Tema-Boreal/Desktop/Imagens/1 - Imagem - homepage2.png";
+        imagem.src = "../Assets/Tema-Boreal/Desktop/Imagens/1 - Imagem - homepage2.png";
         imagem.alt = "Homepage2(Desktop)";
         break;
     }
   }
-}
+}}else if (document.title==="Experiencia"){
+function atualizarImagem(imagem) {
+  const dataImagem = imagem.getAttribute("data-imagem");
+  const larguraTela = window.innerWidth;
 
+  if (larguraTela < 760) {
+    switch (dataImagem) {
+      case "banner":
+        imagem.src = "../Assets/Tema-Boreal/Mobile/Imagens/2 - Banner A Experiência.png";
+        imagem.alt = "Banner(Tablet)";
+        break;
+      case "experiencia1":
+        imagem.src = '../Assets/Tema-Boreal/Mobile/Imagens/2 - A experiência1.png';
+        imagem.alt = "A Experiencia1(Tablet))";
+        break;
+      case "experiencia2":
+        imagem.src = "../Assets/Tema-Boreal/Mobile/Imagens/2 - A experiência2.png";
+        imagem.alt = "A Experiencia2(Tablet)";
+        break;
+      case "experiencia3":
+        imagem.src = "../Assets/Tema-Boreal/Mobile/Imagens/2 - A Experiência3.png";
+        imagem.alt = "A Experiencia3(Tablet)";
+        break;
+    }
+  } else if (larguraTela<1400) {
+    switch (dataImagem) {
+      case "banner":
+        imagem.src = "../Assets/Tema-Boreal/Tablet/Imagens/2 - Banner - A Experiência.png";
+        imagem.alt = "Banner(Tablet)";
+        break;
+      case "experiencia1":
+        imagem.src = '../Assets/Tema-Boreal/Tablet/Imagens/2 - A Experiência1.png';
+        imagem.alt = "A Experiencia1(Tablet)";
+        break;
+      case "experiencia2":
+        imagem.src = "../Assets/Tema-Boreal/Tablet/Imagens/2 - A Experiência2.png";
+        imagem.alt = "A Experiencia2(Tablet)";
+        break;
+      case "experiencia3":
+        imagem.src = "../Assets/Tema-Boreal/Tablet/Imagens/2 - A Experiência3.png";
+        imagem.alt = "A Experiencia3(Tablet)";
+        break;
+    }
+  } else if (larguraTela>1400) {
+    switch (dataImagem) {
+      case "banner":
+        imagem.src = "../Assets/Tema-Boreal/Desktop/Imagens/2 - Banner - A Experiência.png";
+        imagem.alt = "Banner(Desktop)";
+        break;
+      case "experiencia1":
+        imagem.src = '../Assets/Tema-Boreal/Desktop/Imagens/2 - A Experiência1.png';
+        imagem.alt = "A Experiencia1(Desktop)";
+        break;
+      case "experiencia2":
+        imagem.src = "../Assets/Tema-Boreal/Desktop/Imagens/2 - A Experiência2.png";
+        imagem.alt = "A Experiencia2(Desktop)";
+        break;
+      case "experiencia3":
+        imagem.src = "../Assets/Tema-Boreal/Desktop/Imagens/2 - A Experiência3.png";
+        imagem.alt = "A Experiencia3(Desktop)";
+        break;
+    }
+  }
+}}
 
 function atualizarImagens() {
   imagens.forEach(function (imagem) {
